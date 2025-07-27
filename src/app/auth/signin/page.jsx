e7b8439ac9155d4ccfcae9570da45b7deacadd9f
virenishere <virender288@gmail.com>
2025-07-27 10:58:30 +0530

    testing once

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -84,7 +84,8 @@ export default function Login() {
     <div className="min-h-screen flex items-center mt-12 justify-center bg-gradient-to-br from-slate-100 to-slate-200 px-4">
       <div className="w-full max-w-md p-8 bg-white rounded-2xl shadow-xl">
         <h2 className="text-3xl font-bold text-center text-blue-600 mb-6">
-          Login
+          Login 
+          {/* login name */}
         </h2>
 
         {error && (
