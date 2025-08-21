e2d006d47afb6ff65f6a3fff23f7ea0952041112
Yogesh832e <upadhayayyogesh832@gmail.com>
2025-08-21 11:56:43 +0530

    updated srff in db

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -3,23 +3,35 @@ import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 if (!uri) throw new Error("Missing MongoDB URI");
-console.log(uri);
+
+// ------------------
 // Mongoose Connection
+// ------------------
 let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null };
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 export const connectMongoDB = async () => {
   if (cached.conn) return cached.conn;
 
-  const opts = {
-    bufferCommands: false,
-  };
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => mongoose);
+  }
 
-  cached.conn = await mongoose.connect(uri, opts);
-  return cached.conn;
+  try {
+    cached.conn = await cached.promise;
+    return cached.conn;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
 };
 
+// ------------------
 // MongoDB Native Client
+// ------------------
 let client;
 let clientPromise;
 
@@ -35,4 +47,3 @@ if (process.env.NODE_ENV === "development") {
 }
 
 export { clientPromise };
-console.log("Mongoose connection state:", mongoose.connection.readyState);
