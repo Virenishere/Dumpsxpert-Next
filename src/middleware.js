9b56366f6723b9389ceb3cfbdec1877e9cec8e3c
virenishere <virender288@gmail.com>
2025-08-24 12:10:21 +0530

    added debugging logs to auth issue

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(request) {
-  const token = await getToken({ req: request });
+  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = request.nextUrl;
   console.log("[MIDDLEWARE] Path:", pathname, "Token:", token);
 
